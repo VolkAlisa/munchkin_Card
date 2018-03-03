06bb5156c2228db02e6f4c01ec4306081759fa90
VolkAlisa <35622465+VolkAlisa@users.noreply.github.com>
2018-03-03 10:54:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Card
+{
+    class Doors
+    {
+        string name;
+        int level;
+        void condition///условие
+        {
+
+        };
+        void lasciviousness()///непотребство (в случае если проиграешь монстру)
+        {
+
+        };
+        void Anded() ///Вы можете сыграть монстра-Андеда с руки в любой бой с участием Андеда без использования карты Бродячая Тварь
+        {
+
+        };
+        class Class
+        {
+            void capabilities()///Возможности
+            {
+
+            };
+        };
+        class Race
+        {
+            int capabilities;///Возможности
+
+        };
+
+    };
+    class Treasure ///сокровища
+    {
+        void in_hand()
+        {
+            string name;
+            int bonus;
+            void condition() ///условие
+            {
+
+            };
+            int arm;
+            int gold;
+            void escape() ///Cмывка
+            {
+
+            };
+
+        };
+        void armor() ///броня
+        {
+            string name;
+            int bonus;
+            void condition() ///условие
+            {
+
+            };
+            int gold;
+            void escape() ///Cмывка
+            {
+
+            };
+        };
+        void Shoe()
+        {
+            string name;
+            int bonus;
+            struct condition() ///условие
+            {
+
+            };
+            int gold;
+            void escape() ///Cмывка
+            {
+
+            };
+        };
+        void helmet()
+        {
+            string name;
+            int bonus;
+            void condition()///условие
+            {
+
+            };
+            int gold;
+            void escape()///Cмывка
+            {
+
+            };
+
+        };
+        void Level_up()
+        {
+            void condition() ///условие
+            {
+
+            };
+            int level;
+
+        };
+
+    };
+};
